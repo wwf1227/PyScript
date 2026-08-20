a6f0f824c7d880f10b955794730d401cac8f553c
wwf1227 <wwf_weifeng@126.com>
2026-08-20 11:49:18 +0800

    批处理脚本

--- dbqmt/start_xtqmt.bat
+++ dbqmt/start_xtqmt.bat
@@ -1,10 +1,10 @@
 @echo off
 cd /d "%~dp0"
-call venv\Scripts\activate.bat
+call .venv\Scripts\activate.bat
 
 :restart
 echo [%date% %time%] Starting Writer...
-python tick_writer.py
+python xtqmt.py
 echo [%date% %time%] Writer exited. Restarting in 5s... (Ctrl+C to cancel)
 timeout /t 5 /nobreak >nul
 goto restart
